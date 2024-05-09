2f9462d5145c9813701a62706282792ad7520619
vincentiano23 <vincentmuthiani15@gmail.com>
2024-05-09 20:34:18 +0300

    how to style react component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,20 @@
 // import Header from "./Header.jsx";
 // import Footer from "./footer.jsx";  
 // import Food from "./Food.jsx";
-import Card from "./card";
+// import Card from "./card";
+import Button from "./Button.jsx";
 
 function App() {
   return(
     <div>
-      {/* <Header />
+       {/* <Header />
       <Footer />
-      <Food /> */}
+      <Food /> 
       <Card/>
       <Card/>
-      <Card/>
+      <Card/> */}
+      <Button />
+
     </div>
   );
 }
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,17 @@
+function Button() {
+    // Inline styles
+    const styles = {
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    };
+
+    return (
+        <button style={styles}>Click Me</button>
+    );
+}
+
+export default Button;
